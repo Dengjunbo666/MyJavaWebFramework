451cbd9064bfc9392185cc7941a86e2de621d389
dengjunbo <1056233814@qq.com>
2020-12-08 17:28:29 +0800

    第二章 单元测试部分提交

--- chapter2/src/test/java/xyz/dengjunbo/chapter2/test/CustomerServiceTest.java
+++ chapter2/src/test/java/xyz/dengjunbo/chapter2/test/CustomerServiceTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 import xyz.dengjunbo.chapter2.model.Customer;
 import xyz.dengjunbo.chapter2.service.CustomerService;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * CustomerServiceTest 单元测试
@@ -21,8 +24,37 @@ public class CustomerServiceTest {
         // TODO 初始化数据库
     }
     @Test
-    public void getCustomerListTest(){
+    public void getCustomerListTest()throws Exception{
         List<Customer>customerList = customerService.getCustomerList("");
         Assert.assertEquals(2,customerList.size());
+    }
+    @Test
+    public void getCustomerTest()throws Exception{
+        long id = 1;
+        Customer customer = customerService.getCustomer(id);
+        Assert.assertNotNull(customer);
+    }
+    @Test
+    public void createCustomerTest()throws Exception{
+        Map<String,Object>fieldMap = new HashMap<>();
+        fieldMap.put("name","customer100");
+        fieldMap.put("contact","john");
+        fieldMap.put("telephone","13512345678");
+        boolean result = customerService.createCustomer(fieldMap);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void  updateCustomerTest()throws Exception{
+        long id = 1;
+        Map<String,Object> fieldMap = new HashMap<>();
+        fieldMap.put("contact","Eric");
+        boolean result = customerService.updateCustomer(id,fieldMap);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void deleteCustomerTest()throws Exception{
+        long id = 1;
+        boolean result = customerService.deleteCustomer(id);
+        Assert.assertTrue(result);
     }
 }
